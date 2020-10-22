9b01dcf84885c7040d7299959ea927138b3c9360
Inzamamul Qureshi <59908504+InzamamulQureshi@users.noreply.github.com>
2020-10-22 12:42:14 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const discord = require("discord.js")
+const client = new discord.Client()
+client.config = require ("./config.json")
+const fs = require("fs")
+
+let modules = ["utility"] // add , "new category" if you want to add more categories
+
+client.commands = new discord.Collection()
+client.aliases = new discord.Collection()
+
+modules.forEach(module => {
+  fs.readdir(`./commands/${module}/`, (err, files) => { //we need to install fs
+    if(err) console.log(err)
+    files.forEach(f => {
+      if(!f.endsWith(".js")) return;
+      if(f.length <= 0) console.log("No commands found!")
+      console.log(`[ Command ${f} Loaded! ]`)
+      let command = require(`./commands/${module}/${f}`)
+      client.commands.set(command.help.name, command)
+      client.aliases.forEach(alias => {
+        client.aliases.set(alias, command.help.name)
+      })
+    })
+  })
+}) 
+
+fs.readdir("./events/", (err, files) => {
+  if(err) return console.log(err)
+  files.forEach(file => {
+    const event = require(`./events/${file}`)
+    let eventName = file.split(".")[0];
+    console.log(`[ Event Loaded: ${file} ]`)
+    client.on(eventName, event.bind(null, client))
+  })
+})
+
+client.login(client.config.token)
